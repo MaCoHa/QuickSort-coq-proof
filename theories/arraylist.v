aba7d8cd1ac85dd4b60562a76349b0101392e155
Alexander Bilde Pedersen <alexanderbilde@live.dk>
2022-04-24 21:27:48 +0200

    added insert

--- theories/arraylist.v
+++ theories/arraylist.v
@@ -6,3 +6,23 @@ From Coq Require Export Lia.
 From Coq Require Export Lists.List.
 Export ListNotations.
 From Coq Require Export Permutation.
+
+Fixpoint insert (elem : nat) (index : nat) (l : list nat) :=
+match (index, l) with
+| (0, x::xs) => elem :: xs
+| (0, []) => [elem] (* should never happen *)
+| (n, x::xs) => x :: (insert elem (index-1) xs)
+| (n, []) => [elem] (* should never happen *)
+end.
+
+Example insert_example1:
+    insert 3 1 [2;2;2] = [2;3;2].
+Proof.
+    trivial.
+Qed.
+
+Example insert_example2:
+    insert 3 0 [2] = [3].
+Proof.
+    trivial.
+Qed.
