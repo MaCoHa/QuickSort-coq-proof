ae9d3ff1479291ec30adebed247a185c906bbe0f
Alexander Bilde Pedersen <alexanderbilde@live.dk>
2022-05-08 17:02:46 +0200

    still some bugs in quicksort

--- theories/quicksort.v
+++ theories/quicksort.v
@@ -10,6 +10,7 @@ From Coq Require Export Permutation.
 From Coq Require Export Numbers.NatInt.NZDiv.
 Require Coq.Program.Wf.
 
+
 (*Arraylist code*)
 Fixpoint lookup (l : list nat) (i : nat) := 
     match l with
@@ -76,6 +77,7 @@ Proof.
     trivial.
 Qed.
 
+
 Definition shuffle (l : list nat) : list nat :=
     fst3 (List.fold_left (fun (acc : ((list nat)*nat*nat)) (a : nat) =>
             match acc with
@@ -88,18 +90,56 @@ Proof.
     reflexivity.
 Qed.
 
-
-(*Quicksort code*)
-
 Program Fixpoint partition_left (l : list nat) (pivot : nat) (lo : nat) (hi : nat) {measure (hi-lo)} : nat :=
-if ((lookup l lo <? pivot) && (lo <? hi)) then partition_left l pivot (lo+1) hi else lo
-.
+    match hi <=? lo with
+        | true => lo
+        | false => match (lookup l lo <=? pivot) with
+            | true => partition_left l pivot (lo+1) hi
+            | false => lo
+    end end.
+Next Obligation.
+    symmetry in Heq_anonymous0. apply leb_complete_conv in Heq_anonymous0.
+    lia.
+Qed.
+
+Example partition_left_example1:
+    partition_left [1;2;3;4;5] 1 0 4 = 1.
+Proof.
+    reflexivity.
+Qed.
 
 Program Fixpoint partition_right (l : list nat) (pivot : nat) (lo : nat) (hi : nat) {measure (hi-lo)} : nat :=
-    if ((pivot <? lookup l hi ) && (lo <? hi)) then partition_right l pivot lo (hi-1) else hi
-.
+    match hi <=? lo with
+        | true => hi
+        | false => match (pivot <=? lookup l hi) with
+            | true => partition_right l pivot lo (hi-1)
+            | false => hi
+    end end.
+Next Obligation.
+    symmetry in Heq_anonymous0. apply leb_complete_conv in Heq_anonymous0.
+    lia.
+Qed.
 
-Fixpoint partition2 (l : list nat) (pivot : nat) (lo : nat) (hi : nat) : list nat :=
+Example partition_right_example1:
+    partition_right [1;2;3;4;5] 1 0 4 = 0.
+Proof.
+    reflexivity.
+Qed.
+
+Lemma partition_right_le : forall (l : list nat) (pivot lo hi : nat), partition_right l pivot lo hi <= hi.
+Proof. intros. unfold partition_right. unfold partition_right_func.
+Admitted.
+
+Lemma partition_left_ge : forall (l : list nat) (pivot lo hi : nat), partition_left l pivot lo hi >= lo.
+Proof.
+Admitted.
+
+Lemma helper : forall pr pl hi lo, pr <= hi -> pl >= lo -> pl < pr -> pr - 1 - (pl + 1) < hi - lo.
+Proof. intros. lia.
+Qed. 
+
+(* swaps 2 elements that should be swapped *)
+Program Fixpoint partition2 (l : list nat) (pivot : nat) (lo : nat) (hi : nat) {measure (hi-lo)} : list nat :=
     (*
     match (lo,hi) with
     | ((lookup hi l > pivot) && (lookup lo l < pivot)) => partition2 l pivot (lo+1) (hi-1)
@@ -108,9 +148,36 @@ Fixpoint partition2 (l : list nat) (pivot : nat) (lo : nat) (hi : nat) : list na
     | ((lookup hi l < pivot) && (lookup lo l > pivot)) => partition2 (swap l lo hi) pivot (lo+1) (hi-1)
     *)
     match ((partition_left l pivot lo hi), (partition_right l pivot lo hi)) with
-    | (lo, hi) => if lo < hi then partition2 (swap l lo hi) pivot (lo+1) (hi-1) else l
+    | (lo1, hi1) => match hi1 <=? lo1 with
+        | false => match (hi1 <=? hi) with
+            | true => match (lo <=? lo1) with 
+                | true => partition2 (swap l lo1 hi1) pivot (lo1+1) (hi1-1)
+                | false => l (* will never happen *)
+                end
+            | false => l (* will never happen *)
+            end
+        | true => l
+    end end.
+    Next Obligation.
+    (* symmetry in Heq_anonymous. apply leb_complete_conv in Heq_anonymous. assert (partition_right l pivot lo hi <= hi). apply partition_right_le. assert (partition_left l pivot lo hi >= lo). apply partition_left_ge.
+    apply helper. trivial. trivial. trivial. *)
+    symmetry in Heq_anonymous1. apply leb_complete_conv in Heq_anonymous1. symmetry in Heq_anonymous.
+    apply Nat.leb_le in Heq_anonymous. symmetry in Heq_anonymous2. apply Nat.leb_le in Heq_anonymous2. apply helper. trivial. trivial. trivial.
+    Qed.
 
+Definition quicksort (l : list nat) : list nat :=
+    match (shuffle l) with
+    (* | shuffeled => shuffeled  *)
+    | shuffled => partition2 shuffled (lookup shuffled 0) 0 (List.length shuffled - 1)
     end.
+
+Compute (quicksort [5;2;6;3;1;4;9;8;7]).
+
+Example quicksort_example1:
+    quicksort [2;3;1;4] = [2;1;3].
+Proof.
+    reflexivity.
+Qed.
 
 
 
